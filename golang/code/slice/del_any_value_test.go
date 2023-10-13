9cb751122adbbe64c7b915b780f5fe6063ca3a3e
YOUNIZAI <56548528+YOUNIZAI@users.noreply.github.com>
2023-10-13 15:30:49 +0800

    Update del_any_value_test.go
    add delete slice

--- golang/code/slice/del_any_value_test.go
+++ golang/code/slice/del_any_value_test.go
@@ -56,3 +56,53 @@ func TestDelSlice_3(t *testing.T) {
 	b = b[:i]
 	fmt.Printf("b:%v \n",b)
 }
+
+//非排序数组
+//使用 struct{} 节省空间， 指定 cap=len(arr) 避免 map 扩容。记录非重复元素索引 j，将元素前移，原地去重，只需一次遍历。
+//时间复杂度：O(n)
+//空间复杂度：O(n)
+func TestDelSlice_4(t *testing.T) {
+    removeDuplication_map([]string{"1","2","2","3"))
+    removeDuplication_sort([]string{"1","2","2","3"))
+}
+func removeDuplication_map(arr []string) []string {
+    set := make(map[string]struct{}, len(arr))
+    j := 0
+    for _, v := range arr {
+        _, ok := set[v]
+        if ok {
+            continue
+        }
+        set[v] = struct{}{}
+        arr[j] = v
+        j++
+    }
+    return arr[:j]
+}
+
+//已排序数组
+//时间复杂度：O(n)
+//空间复杂度：O(1)
+func removeDuplication_sort(arr []string) []string {
+    length := len(arr)
+    if length == 0 {
+        return arr
+    }
+
+    j := 0
+    for i := 1; i < length; i++ {
+        if arr[i] != arr[j] {
+            j++
+            if j < i {
+                swap(arr, i, j)
+            }
+        }
+    }
+
+    return arr[:j+1]
+}
+
+func swap(arr []string, a, b int) {
+   arr[a], arr[b] = arr[b], arr[a]
+}
+			  
